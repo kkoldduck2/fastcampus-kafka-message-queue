6a92ceed74f484a5384f35cf98b884c4ee86e2f5
kkoldduck2 <vkqmffjtm@naver.com>
2026-02-27 17:42:24 +0900

    spring-kafka 기본설정

--- src/main/java/com/fastcampus/kafkahandson/consumer/MyConsumer.java
+++ src/main/java/com/fastcampus/kafkahandson/consumer/MyConsumer.java
@@ -2,22 +2,23 @@ package com.fastcampus.kafkahandson.consumer;
 
 import ch.qos.logback.core.net.SyslogOutputStream;
 import com.fastcampus.kafkahandson.model.MyMessage;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
 
 @Component
-public class MyConsumer implements Consumer<Message<MyMessage>> {
+public class MyConsumer {
 
-    MyConsumer() {
-        System.out.println("MyCosumer init!");
-    }
-
-    @Override
-    public void accept(Message<MyMessage> message) {
+    @KafkaListener(
+            topics = { "my-json-topic" },
+            groupId = "test-consumer-group"
+    )
+    public void accept(ConsumerRecord<String, MyMessage> message) {
         // 이 컨슈머의 역할은 메시지를 받아서 그 메시지를 그냥 프린트로 찍어주는 역할만 한다.
-        System.out.println("Message arrived! - " + message.getPayload());
+        System.out.println("Message arrived! - " + message.value());
         // TODO message.getHeaders() 도 찍어보기
     }
 }
--- src/main/java/com/fastcampus/kafkahandson/producer/MyProducer.java
+++ src/main/java/com/fastcampus/kafkahandson/producer/MyProducer.java
@@ -1,35 +1,18 @@
 package com.fastcampus.kafkahandson.producer;
 
 import com.fastcampus.kafkahandson.model.MyMessage;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Sinks;
 
-import java.util.function.Supplier;
-
+@RequiredArgsConstructor
 @Component
-public class MyProducer implements Supplier<Flux<Message<MyMessage>>> {
+public class MyProducer {
 
-    MyProducer() {
-        System.out.println("MyCosumer init!");
-    }
-
-    private final Sinks.Many<Message<MyMessage>> sinks = Sinks.many().unicast().onBackpressureBuffer();
+    private final KafkaTemplate<String, MyMessage> kafkaTemplate;
 
     public void sendMessage(MyMessage myMessage) {
-        Message<MyMessage> message = MessageBuilder
-                .withPayload(myMessage)
-                .setHeader(KafkaHeaders.KEY, String.valueOf(myMessage.getAge())) // age 값을 Key로 사용
-                // 같은 age면 항상 같은 파티션으로 감
-                .build();
-        sinks.emitNext(message, Sinks.EmitFailureHandler.FAIL_FAST);
-    }
-
-    @Override
-    public Flux<Message<MyMessage>> get() {
-        return sinks.asFlux();
+        kafkaTemplate.send("my-json-topic", String.valueOf(myMessage.getAge()), myMessage);
     }
 }
+
